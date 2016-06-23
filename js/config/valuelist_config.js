8ba90fe45aee20c65e9cd35b90231f7cdfee6111
Pelletier <578993@bah.com>
2016-06-23 15:53:17 -0400

    Remove '?', which shouldn't be in the base url. It should be added when the url is built.

--- js/config/valuelist_config.js
+++ js/config/valuelist_config.js
@@ -2,5 +2,5 @@
  * Created by 578993 on 6/23/2016.
  */
 angular.module('valueList').config(function(valueListServiceProvider){
-    valueListServiceProvider.setBaseUrl("http://localhost:8080/valueslistservice/values?");
+    valueListServiceProvider.setBaseUrl("http://localhost:8080/valueslistservice/values");
 });
