ff5475bdd3136aa9fe1643b26081df5ebaeec97d
Pelletier <578993@bah.com>
2016-06-24 07:04:39 -0400

    Add simple demo to project.

---
+++ simpleDemo/js/valuelist_config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by 578993 on 6/23/2016.
+ */
+angular.module('valueList').config(function(valueListServiceProvider){
+    valueListServiceProvider.setBaseUrl("http://localhost:8080/valueslistservice/values");
+});
