108d5d564315b81ab99242fc36789ebb1341cc8b
Pelletier <578993@bah.com>
2016-06-28 07:59:38 -0400

    Change response to include all data, just formatted differently. (This may change in the future, it doesn't really add a whole lot, but at the same time isn't very costly)

--- js/config/valuelist_config.js
+++ js/config/valuelist_config.js
@@ -3,5 +3,5 @@
  */
 angular.module('valueList').config(function(valueListServiceProvider){
     //get location of where service is running in tomcat
-    valueListServiceProvider.setServiceUrl("http://localhost:8080/valueslistservice/values");
+    valueListServiceProvider.setServiceUrl("http://localhost:8080/valuelist/valuelistservice/values");
 }).value("defaultParams",{page:1, numberPerPage:15, valueListQuery: 'query'});
